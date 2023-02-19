33e4574e8ad89a2521e28be523ab9c369af91bda
Alekhya Nanduri <alekhyananduri576@gmail.com>
2023-02-19 11:50:21 +0530

    Pipeline Commit

---
+++ main/cclab.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Welcome\n";
+  cout << "PES2UG20CS900 successfully built on pipeline \n";
+  return 0;
+}
